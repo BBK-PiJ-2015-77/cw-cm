5219b0abb8d3dc21950042da00bea9c44cd0ff10
Thomas McGarry <mcgarry_tom@hotmail.com>
2016-02-21 15:01:27 +0000

    Added javadocs

--- ContactImpl.java
+++ ContactImpl.java
@@ -10,8 +10,6 @@ public class ContactImpl implements Contact {
      * @param id    the ID for the contact
      * @param name  the name of the contact
      * @param notes the notes about the contact
-     *
-     * @throws...
      */
 	public ContactImpl(int ID, String name, String notes) {
 		setAge(ID);
@@ -22,6 +20,12 @@ public class ContactImpl implements Contact {
 		//can be done here, do it implementation of CM
 	}
 	
+	/**
+     * Creates a Contact. Name and Notes can be null. ID should be ensured that it is unique before it is passed into this method.
+     *
+     * @param id    the ID for the contact
+     * @param name  the name of the contact
+     */
 	public ContactImpl(int ID, String name) {
 		setAge(ID);
 		this.ID = ID;
@@ -29,27 +33,49 @@ public class ContactImpl implements Contact {
 		this.notes = "";
 	}
 	
+	
+	/**
+	 * @see Contact
+	 */
 	@Override
 	public int getId() {
 		return ID;
 	}
 	
+	
+	/**
+	 * @see Contact
+	 */
 	@Override
 	public String getName() {
 		return name;
 	}
 	
+	
+	/**
+	 * @see Contact
+	 */
 	@Override
 	public String getNotes() {
 		return notes;
 	}
 	
+	
+	/**
+	 * @see Contact
+	 */
 	@Override
 	public void addNotes(String note) {
 		notes = notes + " ++ " + note;
 	}
-	//need to create a list of ID's, to be able to check if they are unique
 	
+	/**
+     * Throws an exception for the ID entered at the constructor if the ID isn't a non-zero positive integer
+     *
+     * @param id    the ID for the contact
+     *
+     * @throws IllegalArgumentException if the ID isn't a non-zero positive integer
+     */
 	private void setAge(int ID) {
 		if (ID <= 0) {
 			throw new IllegalArgumentException("The ID must be a non-zero positive integer");
