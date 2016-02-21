9080a347ec2e8fe46b451ec65a2b5a47305f340e
Thomas McGarry <mcgarry_tom@hotmail.com>
2016-02-21 19:10:25 +0000

    Added javadoc notes and made class abstract

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -1,14 +1,19 @@
 import java.util.Calendar;
 import java.util.Set;
 
-public class MeetingImpl implements Meeting {
+public abstract class MeetingImpl implements Meeting {
 	
 	private int id;
 	private Calendar date;
 	private Set<Contact> contacts;
 	
-	
-	//ID needs to be positive non zero
+	/**
+     * Creates a new MeetingImpl. It should be ensured that ID passed into this is unique elsewhere as this class does not ensure uniqueness.
+     *
+     * @param id        the ID of the meeting
+     * @param date      the date of the meeting
+     * @param contacts  the contacts attending the meeting
+     */
 	public MeetingImpl(int id, Calendar date, Set<Contact> contacts) {
 		ContactImpl.setId(id);
 		this.id = id;
@@ -16,16 +21,28 @@ public class MeetingImpl implements Meeting {
 		this.contacts = contacts;
 	}
 	
+	
+	/**
+	 * @see Meeting
+	 */
 	@Override
 	public int getId() {
 		return id;
 	}
 	
+	
+	/**
+	 * @see Meeting
+	 */
 	@Override
 	public Calendar getDate() {
 		return date;
 	}
 	
+	
+	/**
+	 * @see Meeting
+	 */
 	@Override
 	public Set<Contact> getContacts() {
 		return contacts;
