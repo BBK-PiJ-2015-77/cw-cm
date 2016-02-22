8e9ebe01e78e0af30e572fc3788ec4c7770a73fe
Thomas McGarry <mcgarry_tom@hotmail.com>
2016-02-22 11:23:06 +0000

    First commit

---
+++ PastMeetingImpl.java
@@ -0,0 +1,18 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+	
+	private String notes;
+	
+	public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String notes) {
+		super(id, date, contacts);
+		this.notes = notes;
+	}
+	
+	public String getNotes() {
+		return null;
+	}
+	
+	
+}
