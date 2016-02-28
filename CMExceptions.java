a22bfb81bfa3de885d46ae7abde2bd7a2c7bf84e
Thomas McGarry <mcgarry_tom@hotmail.com>
2016-02-28 17:39:41 +0000

    Added new exception to check if a set of contacts are all contained in another set of contacts

--- CMExceptions.java
+++ CMExceptions.java
@@ -1,4 +1,5 @@
 import java.util.Set;
+import java.util.Iterator;
 
 /**
  * A class to contain all possible exceptions that will be thrown
@@ -45,6 +46,26 @@ public class CMExceptions {
 	public static void setNonEmptySet(Set<Contact> contacts) {
 		if (contacts.isEmpty()) {
 			throw new IllegalArgumentException("An empty list can not be entered as an argument");
+		}
+	}
+	
+	/**
+     * Checks if all the elements of set1 are contained in set 2.
+     * If not, an IllegalArgumentException is thrown.
+     *
+     * @param set1    A Set<Contact> where each element will be inspected to see if its contained in set2
+     * @param set2    A Set<Contact> the reference set to which the elements of set1 are being checked.
+     *
+     * @throws IllegalArgumentException if any element in set1 isn't contained in set2
+     */
+	public static void checkContacts(Set<Contact> set1, Set<Contact> set2) {
+		Iterator<Contact> it = set1.iterator();
+		while (it.hasNext()) {
+			Contact check = it.next();
+			if (set2.contains(check)) { }
+			else {
+				throw new IllegalArgumentException("Contacts must exist on the contact list");
+			}
 		}
 	}
 	
