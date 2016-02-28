6c79977bc6b54c665b1d43c53d0fb275c5815007
Thomas McGarry <mcgarry_tom@hotmail.com>
2016-02-28 17:10:52 +0000

    New tests added for addPastMeetingDate

--- ContactManagerImplTest.java
+++ ContactManagerImplTest.java
@@ -10,37 +10,46 @@ import static org.junit.Assert.assertTrue;
 
 public class ContactManagerImplTest {
 	
-	Calendar date;
+	Calendar pastDate;
 	Calendar currentDate;
+	Calendar futureDate;
 	Set<Contact> contacts;
 	ContactManagerImpl cm;
+	String text;
 	
 	@Before
 	public void setUp() {
-		date = Calendar.getInstance();
-		date.set(2015,12,3);
+		pastDate = Calendar.getInstance();
+		pastDate.set(2015,12,3);
+		
 		currentDate = Calendar.getInstance();
+		
+		futureDate = Calendar.getInstance();
+		futureDate.add(Calendar.DAY_OF_YEAR, +1);
+		
 		contacts = new HashSet<Contact>();
 		Contact con1 = new ContactImpl(1, "Tom", "Good");
 		Contact con2 = new ContactImpl(2, "Tim", "Bad");
 		contacts.add(con1);
 		contacts.add(con2);
+		text = "Meeting notes";
 		
 		cm = new ContactManagerImpl();
 	}
 	
+	/////////addFutureMeeting////////////
+	
 	@Test
 	public void testsaddFutureMeeting() {
-		int aFM = cm.addFutureMeeting(contacts, date);
+		int aFM = cm.addFutureMeeting(contacts, futureDate);
 		assertTrue(aFM>0);
 		//some test that shows an int is returned
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testsfutureMeetingDateAFM() {
-		//IllegalArgumentException if date is in future
-      	currentDate.add(Calendar.DAY_OF_YEAR, +1);
-		cm.addFutureMeeting(contacts, currentDate);
+		//IllegalArgumentException if date is in past
+		cm.addFutureMeeting(contacts, pastDate);
 	}
 	
 	/**
@@ -53,15 +62,68 @@ public class ContactManagerImplTest {
 	
 	@Test(expected=NullPointerException.class)
 	public void testsnullDateAFM() {
-      	date = null;
-		cm.addFutureMeeting(contacts, date);
+      	futureDate = null;
+		cm.addFutureMeeting(contacts, futureDate);
 	}
 	
 	@Test(expected=NullPointerException.class)
 	public void testsnullContactsAFM() {
       	contacts = null;
-		cm.addFutureMeeting(contacts, date);
+		cm.addFutureMeeting(contacts, futureDate);
 	}
 	
+	/////////getPastMeeting////////////
+	
+	/////////getFutureMeeting////////////
+	
+	/////////getMeeting////////////
+	
+	/////////getFutureMeetingList////////////
+	
+	/////////getFutureMeetingList////////////
+	
+	/////////getPastMeetingList////////////
+	
+	/////////addNewPastMeeting////////////
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testsaddPastMeetingDateEmptyContacts() {
+		//IllegalArgumentException if list of contacts is empty
+		contacts.clear();
+		cm.addNewPastMeeting(contacts, pastDate, text);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testsaddPastMeetingDateNullcontacts() {
+		//NullPointerException if date is null
+		contacts = null;
+		cm.addNewPastMeeting(contacts, pastDate, text);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testsaddPastMeetingDateNullpastDate() {
+		//NullPointerException if date is null
+		pastDate = null;
+		cm.addNewPastMeeting(contacts, pastDate, text);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testsaddPastMeetingDateNulltext() {
+		//NullPointerException if text is null
+		text = null;
+		cm.addNewPastMeeting(contacts, pastDate, text);
+	}
+	
+	
+	/////////addMeetingNotes////////////
+
+
+    /////////addNewContact////////////
+
+    /////////getContacts////////////
+
+    /////////getContacts////////////
+    
+    /////////flush////////////
 	
 }
